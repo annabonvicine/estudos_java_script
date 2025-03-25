1602c015f76925bcb159cf3e0b055eb115f82e40
annabonvicine <anna.bonvicine.senai404@gmail.com>
2025-03-25 16:29:35 -0300

    Delete JAVA SCRIPT directory

--- JAVA SCRIPT/exjs/ex04.js
+++
@@ -1,9 +0,0 @@
-let largura = prompt("Digite a largura:  ");
-let altura = prompt("Digite a altura:  ");
-largura && altura == parseFloat (largura && altura);
-
-
-function calculaArea(largura,altura){
-      return largura * altura;
-}
-console.log(calculaArea(largura,altura));
--- JAVA SCRIPT/exjs/ex05.js
+++
@@ -1,7 +0,0 @@
-const frutas = ["Maça", "Banana", "Laranja", "Amora", "Manga"];
-
-console.log("Lista de Frutas:")
-
-for( let i = 0; i < frutas.length; i ++){
-    console.log(frutas[i]);
-}
